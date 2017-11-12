abb0969aa948a5453d9f3ae63d1b9acb30b08fc1
Unknown <edwarfierro9@gmail.com>
2017-11-11 20:35:22 -0500

    prueba

--- src/parques_angiemanrique_eduardfierro_302/Parques_AngieManrique_EduardFierro_302.java
+++ src/parques_angiemanrique_eduardfierro_302/Parques_AngieManrique_EduardFierro_302.java
@@ -19,6 +19,7 @@ public class Parques_AngieManrique_EduardFierro_302 {
     }
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.print("nada");
     }
     
 }
