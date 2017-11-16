b364cf2c71cb0d5677605f4c3bbc4208cb722a29
Steven Hernandez Rios <estevenhr@hotmail.com>
2017-11-15 21:08:12 -0500

    Recomendación
    Recomendaciones sobre el desarrollo del juego. OJO LOS COMMIT DEBEN SER MAS CLAROS !!! no se puede tener un commit con el nombre 3.

---
+++ src/parques_juego/Recomendacion.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parques_juego;
+
+/**
+ *
+ * @author STEVEN
+ */
+public class Recomendacion {
+    /**
+     * Van bien muchachos!!!
+     * 
+     *  Es recomndable en todo desarrollo ir documentando cada clase, método o 
+        variable que vayamos creando, esto con el fin de que documentar no se 
+        vuelva una tarea tediosa al finalizar la aplicación
+
+        Era recomendable se parar por paquetes la parte lógica de la gráfica,
+        esto con el fin de que sea mas claro para cualquier desarrollador
+
+        Les queda mas fácil pintar por coordenadas en una imagen?, no era más fácil
+        crear varios label que formen el tablero, asi no sea todos los pasos, pero 
+        la idea de la imagen como tablero también es valida
+
+        No son necesarios cuatro jugadores, con dos jugadores en suficiente
+     * 
+     * 
+     * MUCHOS ÉXITOS!!!
+     * 
+     */
+    
+
+}
